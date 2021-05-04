788aca208055b9615eea587040749afbc4cc432e
Ashok Mane <ashokmane81800@gmail.com>
2021-05-04 07:16:07 +0530

    [ashok] added a whole binary tree

--- src/test/java/com/bridgelabz/TreeTest.java
+++ src/test/java/com/bridgelabz/TreeTest.java
@@ -15,4 +15,23 @@ public class TreeTest {
        tree.insert(node,70);
         Assert.assertEquals(56,node.value);
     }
+
+    @Test
+    public void creatingWholeBinaryTree() {
+        Node node = new Node(56);
+        Tree tree = new Tree();
+        tree.insert(node,30);
+        tree.insert(node,70);
+        tree.insert(node,22);
+        tree.insert(node,40);
+        tree.insert(node,11);
+        tree.insert(node,3);
+        tree.insert(node,16);
+        tree.insert(node,60);
+        tree.insert(node,95);
+        tree.insert(node,65);
+        tree.insert(node,63);
+        tree.insert(node,67);
+        Assert.assertEquals(56,node.value);
+    }
 }
